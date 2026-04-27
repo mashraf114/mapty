6d21123ec231ca4813e7f0fc4a05fec26b651460
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-04-27 14:42:05 +0300

    update: cycling icon

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ class App {
           <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
             <span class="workout__icon">${
-              workout.type === 'running' ? '🏃‍♂️' : '⏱'
+              workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
             }</span>
             <span class="workout__value">${workout.distance}</span>
             <span class="workout__unit">km</span>
